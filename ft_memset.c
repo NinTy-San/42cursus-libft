77142647343dcc4f7429acd67719b94044130c9f
Arnaud DOHOU <adohou@e2r12p3.clusters.42paris.fr>
2022-05-09 14:35:36 +0200

    add ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,34 +6,33 @@
 /*   By: adohou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 21:08:34 by adohou            #+#    #+#             */
-/*   Updated: 2022/05/08 21:41:22 by adohou           ###   ########.fr       */
+/*   Updated: 2022/05/09 14:33:59 by adohou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libft.h"
 
-void * ft_memset( void * s, int c, size_t n )
+void	*ft_memset(void *s, int c, size_t n)
 {
-    int *ptr;
+	char	*ptr;
+	size_t	i;
 
-    ptr = (int*) s;
-
-    if(!s)
-        return(NULL);
-    while(n)
-            ptr[n--] = c;
-    return(ptr);
+	ptr = (char *) s;
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (i < n)
+		ptr[i++] = c;
+	return (ptr);
 }
 
-int	main()
+/*int     main()
 {
-	char str[] = "abcdef";	
+        char str[] = "it was all a dream";  
 
-	printf("\n memset() = %p", memset(str, '?', 3));	
-	printf("\n ft_memset() = %p", ft_memset(str, '?', 3));	
-}
-
+      printf("\n memset() =    %p", memset(str, '?', 3));        
+      printf("\n ft_memset() = %p \n", ft_memset(str, '?', 3));  
+}*/
 
 /*void ft_bzero(void * s , size_t n    )
 {
