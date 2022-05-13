5de8f0bd7a5000e4aaab337bad50a1d762e0d76d
Arnaud DOHOU <adohou@e2r4p11.clusters.42paris.fr>
2022-05-13 18:47:36 +0200

    add ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: adohou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 17:00:01 by adohou            #+#    #+#             */
-/*   Updated: 2022/05/12 18:21:44 by adohou           ###   ########.fr       */
+/*   Updated: 2022/05/13 18:45:54 by adohou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,30 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	const char	*sc;
-	char	*dst;
-	
-	sc = src;
-	dst = dest;
-	
-	if (dst < sc)
-		ft_memcpy(dst, sc, n);
+	const char	*s;
+	char		*d;
+	char		*tmpdest;
+
+	s = src;
+	d = dest;
+	if (d < s)
+		ft_memcpy(d, s, n);
 	else
 	{
-		char	*tmp;	
-		sc += (n - 1);
-		dst += (n - 1);
-		tmp = NULL;
+		s = s + (n -1);
+		tmpdest = d + (n -1);
 		while (n--)
 		{
-			*tmp = *sc--;
-			*dst-- = *tmp++;
+			*tmpdest-- = *s--;
 		}	
-		
 	}
-	return (dst);	
+	return (d);
 }
 
-int 	main()
+/*int 	main()
 {
 	const char	src[] = "salut";
-//	char *dest = NULL;
+	char dest[] = "it was all a dream";
 
-	printf("\n ft_memmove = %s \n", (char *) ft_memmove((char *)src + 2, src, 4));
-}
+	printf("\n ft_memmove = %s \n", (char *) ft_memmove(dest, src, 4));
+}*/
