cae22b98a0abc51a5d6715c1b31bcde07d145f80
Arnaud DOHOU <adohou@e2r11p1.clusters.42paris.fr>
2022-05-16 20:50:45 +0200

    add ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adohou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 19:58:07 by adohou            #+#    #+#             */
+/*   Updated: 2022/05/16 20:33:09 by adohou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	mlen;
+
+	mlen = nmemb * size;
+	ptr = malloc(mlen);
+	if (!mlen)
+		return (NULL);
+	ft_bzero(ptr, mlen);
+	return (ptr);
+}
+
+/*#include <assert.h>
+
+#define INT_NUMBER 10
+
+int main() {
+    
+    int i;
+    int * pointer = (int *) ft_calloc( INT_NUMBER, sizeof(int) );
+
+    // Un petit test sur le bon fonctionnement de calloc 
+    // assert( pointer != NULL );
+    // Le dernier élément du tableau ne sera pas affecté par la 
+    // boucle mais n'oubliez pas que calloc initialise de toute 
+    // façon à 0 chaque octet. 
+    
+    for ( i=0; i<INT_NUMBER-1; i++ ) {
+        pointer[i] = i;
+    }
+
+    // On affiche le contenu du tableau d'entier
+    // Ce qui donne : 0 1 2 3 4 5 6 7 8 0 
+    for (i=0; i<INT_NUMBER; i++ ) {
+        printf( "%d ", pointer[i] );
+    }
+    printf( "\n" );
+
+    // On libère le bloc de mémoire alloué dynamiquement
+    free( pointer );
+
+    return 0;
+}*/
