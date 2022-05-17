9e76957f9135bba9df645b4bc0d0558e7fef89b4
Arnaud DOHOU <adohou@e2r13p1.clusters.42paris.fr>
2022-05-17 18:08:51 +0200

    add ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adohou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/17 16:57:03 by adohou            #+#    #+#             */
+/*   Updated: 2022/05/17 18:07:23 by adohou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, const char *s2)
+{
+	char	*newstr;
+	size_t	lens1;
+	size_t	lens2;
+	int		i;
+	int		j;
+
+	lens1 = ft_strlen((char *) s1);
+	lens2 = ft_strlen((char *) s2);
+	newstr = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (!newstr)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (lens1--)
+		*(newstr + (j + 1)) = *(s1 + (j + 1));
+	while (lens2--)
+		*(newstr + j++) = *(s2 + i++);
+	*(newstr + j) = '\0';
+	return (newstr);
+}
+
+/*int	main()
+{
+	printf("new str = %s \n", ft_strjoin((char *)"it's ", "all good"));
+}*/
