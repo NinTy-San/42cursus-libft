43c889d1c23c1550a6dc9e3be0cb858bb4afa251
Arnaud DOHOU <adohou@e2r9p1.clusters.42paris.fr>
2022-05-20 22:53:32 +0200

    push

--- ft_calloc.c
+++ ft_calloc.c
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	mlen = nmemb * size;
 	ptr = malloc(mlen);
-	if (!mlen)
+	if (!ptr)
 		return (NULL);
 	ft_bzero(ptr, mlen);
 	return (ptr);
@@ -30,22 +30,22 @@ void	*ft_calloc(size_t nmemb, size_t size)
 #define INT_NUMBER 10
 
 int main() {
-    
+
     int i;
     int * pointer = (int *) ft_calloc( INT_NUMBER, sizeof(int) );
 
-    // Un petit test sur le bon fonctionnement de calloc 
+    // Un petit test sur le bon fonctionnement de calloc
     // assert( pointer != NULL );
-    // Le dernier élément du tableau ne sera pas affecté par la 
-    // boucle mais n'oubliez pas que calloc initialise de toute 
-    // façon à 0 chaque octet. 
-    
+    // Le dernier élément du tableau ne sera pas affecté par la
+    // boucle mais n'oubliez pas que calloc initialise de toute
+    // façon à 0 chaque octet.
+
     for ( i=0; i<INT_NUMBER-1; i++ ) {
         pointer[i] = i;
     }
 
     // On affiche le contenu du tableau d'entier
-    // Ce qui donne : 0 1 2 3 4 5 6 7 8 0 
+    // Ce qui donne : 0 1 2 3 4 5 6 7 8 0
     for (i=0; i<INT_NUMBER; i++ ) {
         printf( "%d ", pointer[i] );
     }
