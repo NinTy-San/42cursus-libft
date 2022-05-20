e1c037e6f3f376942fb395a7b2643790638a9558
Arnaud DOHOU <adohou@e2r9p1.clusters.42paris.fr>
2022-05-20 22:23:09 +0200

    fixed : ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -23,10 +23,15 @@ int	ft_atoi(const char *nptr)
 	sign = 1;
 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	if (nptr[i++] == '-')
-		sign *= -1;
-	while (nptr[i] != '\0' && (nptr[i] >= '0' && nptr[i] <= '9'))
+	if (nptr[i] == '-' || nptr[i] == '+')
 	{
+		if (nptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (nptr[i] && (nptr[i] >= '0' && nptr[i] <= '9'))
+	{
+
 		nb = nb * 10 + nptr[i] - '0';
 		i++;
 	}
