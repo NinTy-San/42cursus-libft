5bb6e88a7f50a3ac6038681c032efce52fcd27b5
Arnaud DOHOU <adohou@e2r12p22.clusters.42paris.fr>
2022-05-27 22:56:44 +0200

    updated split

--- ft_split.c
+++ ft_split.c
@@ -11,85 +11,85 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	chech_char(char c, char d)
+static int    chech_char(char c, char d)
 {
-	if (c == d)
-		return (1);
-	return (0);
+    if (c == d)
+        return (1);
+    return (0);
 }
-static int	nb_elem(const char *s, char del)
+static int    nb_elem(const char *s, char del)
 {
-	int	i;
-	int	elem;
+    int    i;
+    int    elem;
 
-	i = 0;
-	elem = 0;
-	while (s[i])
-	{
-		if (chech_char(s[i], del))
-		{
-			while (chech_char(s[i], del))
-				i++;
-			elem++;
-		}
-		else
-			i++;
-	}
-	if (!elem)
+    i = 0;
+    elem = 0;
+	if (del == 0)
 		elem++;
-	return (elem);
+    while (s[i])
+    {
+        if (chech_char(s[i], del))
+        {
+            while (chech_char(s[i], del))
+                i++;
+            if (s[i])
+            elem++;
+        }
+        else
+            i++;
+    }
+    return (elem);
 }
 
-static char	*set_elem(const char *str, char c, int *p)
+static char    *set_elem(const char *str, char c, int *p)
 {
-	char	*elem;
-	int	i;
-	int	j;
-	int	k;
+    char    *elem;
+    int    i;
+    int    j;
+    int    k;
 
-	i = 0;
-	j = 0;
-	while (chech_char(str[*p], c))
-		*p = *p + 1;
-	k = *p;
-	while (!chech_char(str[*p], c))
-	{
-		i++;
-		*p = *p + 1;
-	}
-	elem = (char *) malloc(sizeof(char) * i + 1);
-	while (j < i)
-		elem[j++] = str[k++];
-	elem[j] = '\0';
-	return (elem);
+    i = 0;
+    j = 0;
+    while (chech_char(str[*p], c))
+        *p = *p + 1;
+    k = *p;
+    while (str[*p] && !chech_char(str[*p], c))
+    {
+        i++;
+        *p = *p + 1;
+    }
+    elem = (char *) malloc(sizeof(char) * i + 1);
+    while (j < i)
+        elem[j++] = str[k++];
+    elem[j] = '\0';
+    return (elem);
 }
 
-char	**ft_split(const char *s, char c)
+char    **ft_split(const char *s, char c)
 {
-	char **splitTab;
-	int	i;
-	int j;
-	int	len;
+    char **splitTab;
+    int    i;
+    int j;
+    int    len;
 
-	len = nb_elem(s, c);
-	splitTab = malloc(sizeof(char *) * len);
-	i = 0;
-	j = 0;
-	while (i < len - 1)
-		splitTab[i++] = set_elem(s, c, &j);
-	splitTab[i] = NULL;
-	return(splitTab);
+    len = nb_elem(s, c);
+    splitTab = malloc(sizeof(char *) * (len + 1));
+    i = 0;
+    j = 0;
+    while (len && i < len)
+        splitTab[i++] = set_elem(s, c, &j);
+    splitTab[len] = NULL;
+    return(splitTab);
 }
 
-int	main()
+/* int    main()
 {
-	char	**split;
-	int	i;
+    char    **split;
+    int    i;
 
-	i = 0;
-	split = ft_split("tripouille", 0);
-	while (split[i])
-		printf("elem = %s \n", split[i++]);
-	free(split);
-}
+    i = 0;
+    split = ft_split("tripouille", 0);
+    while (split[i])
+        printf("elem = %s \n", split[i++]);
+    free(split);
+} */
